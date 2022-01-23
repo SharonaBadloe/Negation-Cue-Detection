16b0d2d0d5ac609724f4c90b41123224423c0317
MichielvNederpelt <61193709+MichielvNederpelt@users.noreply.github.com>
2022-01-23 15:50:51 +0100

    Add files via upload

--- feature_extraction.py
+++ feature_extraction.py
@@ -13,8 +13,8 @@ inputfile_dev = 'SEM-2012-SharedTask-CD-SCO-dev-simple.v2.txt'
 def add_features(inputfile):
     '''
     Takes as input a .txt file consisting of a tokenized corpus. Converts the corpus into a pandas df for feature extracting.
-    Extracts the following features from the data: 
-        
+    Extracts the following features from the data:
+
     Token i-1
     Token i+1
     Lemma i
@@ -29,17 +29,17 @@ def add_features(inputfile):
     hasNegAffix
     Negated event
     Negational affix
-    
+
     :param inputfile: .txt file
     :returns: .conll file with added features
-    '''    
+    '''
     # open inputfile in pandas dataframe
     # add column headers
     df = pd.read_csv(inputfile, sep="\t", header=None, names=["Book", "Sent nr", "Token nr", "Token", "Label"])
-    
+
     # put all our tokens into a list:
     tokenlist = df['Token'].tolist()
-    
+
     # put all labels into a list
     label_list = df['Label'].tolist()
 
@@ -56,24 +56,24 @@ def add_features(inputfile):
             nexttoken.append(tokenlist[i+1])
         elif i == len(tokenlist) - 1:
             pretoken.append(tokenlist[i-1])
-            nexttoken.append('<EOD>')                
+            nexttoken.append('<EOD>')
         else:
             pretoken.append(tokenlist[i-1])
             nexttoken.append(tokenlist[i+1])
-        i += 1   
+        i += 1
 
     # add token features to dataframe
     df['Pre token'] = pretoken
     df['Next token'] = nexttoken
 
-    # extract lemma    
+    # extract lemma
     # iterate over tokens and extract lemma
-    d = [] 
+    d = []
     for token in tokenlist:
         doc = nlp(token)
         d.append(doc[0].lemma_)
     df['Lemma'] = d     # add lemma to dataframe
-    
+
     # extract previous and next lemma
     lemmalist = df['Lemma'].tolist()
     pre_lemma = []
@@ -87,22 +87,22 @@ def add_features(inputfile):
             next_lemma.append(lemmalist[i+1])
         elif i == len(lemmalist) - 1:
             pre_lemma.append(lemmalist[i-1])
-            next_lemma.append('<EOD>')                
+            next_lemma.append('<EOD>')
         else:
             pre_lemma.append(lemmalist[i-1])
             next_lemma.append(lemmalist[i+1])
-        i += 1   
+        i += 1
 
     # add previous and next lemma to dataframe
     df['Pre lemma'] = pre_lemma
     df['Next lemma'] = next_lemma
 
     # extract pos tags
-    d = [] 
+    d = []
     for token, pos in nltk.pos_tag(tokenlist):
         d.append(pos)
     df['POS'] = d   # add postags to dataframe
-    
+
     # extract previous and next postags
     # create two lists for collecting previous tag and next tag
     poslist = df['POS'].tolist()
@@ -117,30 +117,67 @@ def add_features(inputfile):
             next_pos.append(poslist[i+1])
         elif i == len(tokenlist) - 1:
             pre_pos.append(poslist[i-1])
-            next_pos.append('<EOD>')                
+            next_pos.append('<EOD>')
         else:
             pre_pos.append(poslist[i-1])
             next_pos.append(poslist[i+1])
-        i += 1   
+        i += 1
 
     # add previous and next pos to dataframe
     df['Pre POS'] = pre_pos
     df['Next POS'] = next_pos
 
-    # extract punctuation
+# code for classifying POS-tags
+pos_cat_list = []
+for tag in poslist:
+    if tag == "VERB" or tag == "AUX":
+        pos_cat_list.append("VB")
+    elif tag == "PRON":
+        pos_cat_list.append("PRO")
+    elif tag == "DET" or tag == "X" or tag == "NUM" or tag == "INTJ":
+        pos_cat_list.append("OTH")
+    elif tag == "ADV" or tag == "ADP" or tag == "SCONJ" or tag == "PART":
+        pos_cat_list.append("ADVB")
+    elif tag == "PROPN" or tag == "NOUN":
+        pos_cat_list.append("NN")
+    #if anything else, not seen in trainingdata comes forward:
+    else:
+        pos_cat_list.append("OTH")
+
+train_df['POS_classified'] = pos_cat_list
+
+    #code for extracting punctuation marks
     d = []
-    l = string.punctuation
-    for token in tokenlist:
-        if token in l:
+    print("processing words with spacy")
+    for word in tokenlist:
+        doc = nlp(word)
+        punctuation_list = []
+        for item in doc:
+            if item.is_punct == True:
+                punctuation_list.append("1")
+            if item.is_punct == False:
+                punctuation_list.append("0")
+            else:
+                print("Error: check word processing")
+                break
+        d.append(punctuation_list)
+    train_df['Punctuation_spacy'] = d
+
+    #other option for extracting punctuation marks without the use of spacy
+
+    d = []
+    punct_list = string.punctuation
+    for token in df['Token']:
+        if token in punct_list:
             d.append(1)
         else:
             d.append(0)
-    df['Punct'] = d   # add punctuation feature to dataframe
-    
+    df['Punctuation_python'] = d
+
     # extract MatchesNegExp feature
     # define NegExpList
     neg_exp = ['nor', 'neither', 'without', 'nobody', 'none', 'nothing', 'never', 'not', 'no', 'nowhere', 'non']
-    
+
     # iterate over tokens and check if they match the NegExpList
     matchesnegexp = []
     for token in tokenlist:
@@ -148,8 +185,8 @@ def add_features(inputfile):
             matchesnegexp.append(1)
         else:
             matchesnegexp.append(0)
-    df['MatchesNegExp'] = matchesnegexp  # add feature to dataframe 
-    
+    df['MatchesNegExp'] = matchesnegexp  # add feature to dataframe
+
     # extract negational affix features
     # create list of positive vocabulary
     positives = []
@@ -158,7 +195,7 @@ def add_features(inputfile):
         if item == 'O':
             positives.append(tokenlist[i].lower())  # lowercase tokens for lookup
         i += 1
-        
+
     # assign lists for each of the three features
     hasnegaffix = []
     negated_event = []
@@ -173,8 +210,8 @@ def add_features(inputfile):
                 negated_event.append(split_token)    #  add negated event to list
                 neg_affix.append('un')        # add affix to affix list
                 hasnegaffix.append(1)         #  add 1 to binary affix list
-            else:                      
-                negated_event.append('-')   # if token is a positive word: 
+            else:
+                negated_event.append('-')   # if token is a positive word:
                 neg_affix.append('-')       # add dashes and 0 to appropriate lists
                 hasnegaffix.append(0)
         elif token.startswith('dis'):
@@ -186,7 +223,7 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
+                hasnegaffix.append(0)
         elif token.startswith('im'):
             split_token = token[2:]
             if token not in positives:
@@ -196,7 +233,7 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
+                hasnegaffix.append(0)
         elif token.startswith('in'):
             split_token = token[2:]
             if token not in positives:
@@ -206,7 +243,7 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
+                hasnegaffix.append(0)
         elif token.startswith('non'):
             split_token = token[3:]
             if token not in positives:
@@ -216,7 +253,7 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
+                hasnegaffix.append(0)
         elif token.startswith('ir'):
             split_token = token[2:]
             if token not in positives:
@@ -226,7 +263,7 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
+                hasnegaffix.append(0)
         elif token.endswith('less'):
             split_token = token[:-4]
             if token not in positives:
@@ -236,7 +273,7 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
+                hasnegaffix.append(0)
         elif token.endswith('lessly'):
             split_token = token[:-6]
             if token not in positives:
@@ -246,7 +283,7 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
+                hasnegaffix.append(0)
         elif token.endswith('lessness'):
             split_token = token[:-8]
             if token not in positives:
@@ -256,24 +293,24 @@ def add_features(inputfile):
             else:
                 negated_event.append('-')
                 neg_affix.append('-')
-                hasnegaffix.append(0) 
-        else:                          
+                hasnegaffix.append(0)
+        else:
             negated_event.append('-')  # if token does not have any negational affixes:
             neg_affix.append('-')      # add dashes and 0 to appropriate lists
             hasnegaffix.append(0)
-    
+
     # add affixal negation features to dataframe
     df['HasNegAffix'] = hasnegaffix
     df['Negated event'] = negated_event
     df['NegAffix'] = neg_affix
-    
+
     # change filename and write data to .conll file
-    outputfile = inputfile.replace('.txt','.features.conll')   
+    outputfile = inputfile.replace('.txt','.features.conll')
     df.to_csv(outputfile, sep='\t', header=True, quotechar='|', index=False)
-    
+
     outputfile2 = inputfile.replace('.txt', '.features.csv')
     df.to_csv(outputfile2, sep='\t', header=True, quotechar='|', index=False)
-    
+
 # function calls
 add_features(inputfile_train)
 add_features(inputfile_dev)
